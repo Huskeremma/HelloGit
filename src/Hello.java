5998b807b62eca9798e4a0f2210a8423c5c32c83
Logan Harris <lharris12@huskers.unl.edu>
2023-08-28 22:32:54 -0500

    Added my name and major

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("Emma Walter");
     System.out.println(" Computer engineering<3");
+    
+    System.out.println("Logan Harris");
+    System.out.println("Computer science!");
   }
 
 }
